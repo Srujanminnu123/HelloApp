cb2f013cc104add91e6395a09be25cae1681ddf3
Srujanminnu123 <kk9895@srmist.edu.in>
2026-03-25 08:17:13 +0530

    Add UC7 file only

---
+++ HelloAppuc7.java
@@ -0,0 +1,13 @@
+public class HelloAppuc7 {
+    public class Main {
+    public static void main(String[] args) {
+        
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+            String names = String.join(", ", args);
+            System.out.println("Hello, " + names + "!");
+        }
+    }
+}
+}
